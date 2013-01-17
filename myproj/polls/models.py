134c78aa7b75d3c76b5b55d5a53265101c6e496e
Joshua Rumbut <joshua.rumbut@gmail.com>
2013-01-16 21:38:55 -0500

    fixes was_published_recently

--- myproj/polls/models.py
+++ myproj/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime,timedelta
+from django.utils import timezone
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
@@ -8,7 +10,11 @@ class Poll(models.Model):
         return self.question
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - timedelta(days=1)
+    
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
     
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
